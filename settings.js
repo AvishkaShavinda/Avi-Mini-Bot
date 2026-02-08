bfbf2dcebca16715b54c813fbd5644ff479aaacf
AVISHKA SHAVINDA <134716496+AvishkaShavinda@users.noreply.github.com>
2026-02-08 08:52:42 +0530

    Add initial settings configuration in settings.js

---
+++ settings.js
@@ -0,0 +1,8 @@
+module.exports = {
+    AUTO_STATUS_VIEW: true, // Status ඉබේ බැලීමට true දාන්න
+    OWNER_NUMBER: '947xxxxxxxxx',
+    BOT_NAME: 'AVI H4CK3R',
+// ... අනෙක් settings
+    MENU_IMAGE_URL: 'https://files.catbox.moe/jggkxf.jpg', // මෙතනට ඔයාගේ Image URL එක දාන්න
+    // උදාහරණයක් ලෙස: 'https://i.imgur.com/xxxxxx.jpg'
+};
